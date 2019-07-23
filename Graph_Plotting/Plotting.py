f8d3ba685c31af572246e60a3980fe00c406f7c9
hmchurn <48350986+hmchurn@users.noreply.github.com>
2019-07-23 17:22:55 +0100

    stats.sem to stats.tstd

--- Graph_Plotting/Plotting.py
+++ Graph_Plotting/Plotting.py
@@ -58,7 +58,7 @@ class Plot:
         """
         # A very rough method of trying to ignore anomalous points in the It data
         mean_current= sum(it.i_mean)/len(it.i_mean)
-        mean_error = stats.sem(it.i_mean)
+        mean_error = stats.tstd(it.i_mean)
         main_data_i = []
         main_data_t = []
         main_data_ierror =[]
@@ -66,7 +66,7 @@ class Plot:
         bad_data_t = []
         bad_data_ierror = []
         for i in range(len(it.i_mean)):
-            if mean_current+(100 * mean_error) > it.i_mean[i] > mean_current - (100* mean_error):
+            if mean_current+(3 * mean_error) > it.i_mean[i] > mean_current - (3* mean_error):
                 main_data_t.append(it.time[i])
                 main_data_i.append(it.i_mean[i])
                 main_data_ierror.append(it.i_error[i])
